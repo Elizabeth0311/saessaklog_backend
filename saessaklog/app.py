38c93f25966630511557e01668a293e7b66fec96
HyeRi <64198864+Elizabeth0311@users.noreply.github.com>
2023-02-21 19:41:11 +0900

    Add files via upload

--- saessaklog/app.py
+++ saessaklog/app.py
@@ -1,9 +1,64 @@
-from flask import Flask
+
+from flask import Flask, render_template, request ,flash
+from werkzeug.utils import secure_filename
+from s3_conn import upload_file_to_s3 ,s3_connection
+from config import S3_BUCKET_NAME 
+import datetime as dt
+import os
+
+
+
 app = Flask(__name__)
 
-@app.route("/main")
-def hello() :
-    return "Hello, World!"
+# @app.route("/main")
+# def hello() :
+#     return "Hello, World!"
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+# 확장자 확인
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        
+
+
+@app.route("/file_upload", methods = ['GET', 'POST'])
+def upload_file() :
+    if request.method == 'POST' :
+        file = request.files['file']
+        file_name = file.filename.split('.')[0] # 파일명 
+        ext = file.filename.split('.')[-1] # 확장자명 
+        img_name = dt.datetime.now().strftime(f"{file_name}-%Y-%m-%d-%H-%H-%S.{ext}")
+        print(img_name)
+        print("==============")
+        print(type(file))
+        s3 = s3_connection
+        upload_file_to_s3(s3,'saessalogfile',file, img_name)
+        
+        return '파일이 저장되었습니다'
+
+    else :
+        return render_template("file_upload.html")
+'''
+출처 https://gist.github.com/leongjinqwen/9d9a2d58bf2fb923658820559a88a5ec
+'''
+
+
+# @app.route("/file_upload", methods = ['GET', 'POST'])
+# def file_upload() :
+#     if request.method == 'POST' :
+#         f = request.files['file'] # http에서 파일가져오기
+#         # filename = f.save(secure_filename(f.filename))
+#         filename = secure_filename(f.filename)
+#         f.save(os.path.join('/Users/hyeri/Downloads/s3_save',filename))
+        
+ 
+
+    #     return '파일이 저장되었습니다.'
+    # else : 
+    #     return render_template("file_upload.html")
 
 if __name__ == "__main__" :
+    app.secret_key = "saessak_servser"
     app.run(host='0.0.0.0', port = 5000, debug=False)
---
+++ saessaklog/s3_conn.py
@@ -0,0 +1,38 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+from werkzeug.utils import secure_filename
+from config import ACCES_KEY_ID, SECRET_ACCEES_KEY, S3_BUCKET_REGION
+import os
+
+
+
+def s3_connection() :
+    try : 
+        s3 = boto3.client(
+            service_name = 's3',
+            region_name = S3_BUCKET_REGION,
+            aws_access_key_id = ACCES_KEY_ID,
+            aws_secret_access_key = SECRET_ACCEES_KEY,
+    )
+    except Exception as e :
+        print(e)
+    else : 
+        print("s3 bucket connected!")
+        return s3
+
+s3 = s3_connection()
+
+def upload_file_to_s3(s3, bucket, file, filename) :
+    try :
+        s3.put_object(
+            Body = file,
+            Bucket = bucket,
+            Key = f'image/{filename}',
+            ContentType = file.content_type,
+            ACL = "public-read" #공개범위설정
+        )
+
+    except Exception as e :
+        return  False
+    return True
